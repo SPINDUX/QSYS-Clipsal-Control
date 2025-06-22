9c01a077c7067286d87024950f05ba29cf32758b
SPINDUX <107009854+SPINDUX@users.noreply.github.com>
2025-06-22 14:08:35 +1200

    Rename plugin to Clipsal C-BUS Serial

--- src/info.lua
+++ src/info.lua
@@ -1,9 +1,9 @@
 PluginInfo = {
-  Name = "TAG~Dynalite~Envision Gateway",
+  Name = "TAG~Clipsal C-BUS Serial",
   Version = "2.5",
   BuildVersion = "2.5.0.1",
   Id = "6add4168-6655-4304-9f33-2a756b485f67",
-  Description = "A Q-SYS plugin for the Phillips Dynalite Ethernet Gateway (PDEG) and Phillips Dynalite Ethernet Bridge (PDEB).",
+  Description = "A Q-SYS plugin for Clipsal C-BUS Serial integration.",
   ShowDebug = true,
   Author = "Jason Foord"
 }
